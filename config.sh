c7695ea0a64a6d2a26051bcf4b13c4ca72322dd8
Jiri Stransky <jstransk@redhat.com>
2012-08-13 15:12:24 +0200

    Change default ruby to 1.8

--- config.sh
+++ config.sh
@@ -2,7 +2,8 @@
 export dev_user=aedev
 export dev_home="/home/$dev_username"
 
-export ruby_version='1.9.3-p194'
+export ruby_version='1.8.7-p370'
+# export ruby_version='1.9.3-p194'
 
 export source_install_dir="$dev_home"
 
