ac83d0ec4eb2087068dadf9913d3d2c54d2f8033
Jiri Stransky <jstransk@redhat.com>
2012-08-15 10:50:45 +0200

    add commented-out attempt at installing Ruby 1.8.7
    I cannot get it to work now, it won't be easy to get rbenv + Ruby 1.8 on
    Fedora 17.
    
    However, there seems to be a fix when using RVM:
    http://shlrm.org/blog/2012/07/02/building-ruby-1-dot-8-7-on-fedora-17-using-rvm/
    
    Unfortunately rbenv doesn't support patching in the process of Ruby
    installation. I guess Ruby would have to be built with manual
    interception. This might be useful:
    http://stackoverflow.com/questions/6134456/error-while-installing-ruby-1-8-7-on-fedora-15

--- common/rbenv-ruby.sh
+++ common/rbenv-ruby.sh
@@ -12,3 +12,31 @@ if ! which rbenv &> /dev/null ; then
 fi
 
 rbenv install "$ruby_version"
+
+# if [[ "$ruby_version" =~ 1\.8\.7 ]] ; then
+#   gcc_dir=/usr/local/gcc45
+#   echo "Installing GCC 4.5 into $gcc_dir to be able to build Ruby 1.8.7"
+#
+#   echo "Installing build deps..."
+#   yum install -y gcc mpfr-devel libmpc libmpc-devel glibc-devel
+#
+#   mkdir -p "$gcc_dir/source"
+#   cd "$gcc_dir/source"
+#
+#   echo "Downloading..."
+#   wget http://gcc.fyxm.net/releases/gcc-4.5.4/gcc-4.5.4.tar.bz2
+#
+#   echo "Extracting..."
+#   tar -xjf gcc-4.5.4.tar.bz2
+#
+#   echo "Compiling..."
+#   cd "$gcc_dir/source/gcc-4.5.4"
+#   ./configure --prefix="$gcc_dir" --program-suffix=45 --enable-languages=c,c++ &&
+#   make &&
+#   make install
+#
+#   CC=/usr/local/gcc45/bin/gcc45 rbenv install "$ruby_version"
+# else
+#   rbenv install "$ruby_version"
+# fi
+
