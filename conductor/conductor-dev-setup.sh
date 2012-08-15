fe62477172678e3d7e995cf46b1204490f98a86c
Jiri Stransky <jstransk@redhat.com>
2012-08-15 11:48:41 +0200

    Make conductor install script work with config.sh

--- conductor/conductor-dev-setup.sh
+++ conductor/conductor-dev-setup.sh
@@ -6,28 +6,26 @@
 # navigate to the config file regardless of current working directory
 source "`dirname \"$0\"`/../config.sh"
 
-mkdir -p $source_install_dir
-cd $source_install_dir
 git clone git://github.com/aeolusproject/conductor.git
-cd $source_install_dir/conductor
+cd "$conductor_dir"
 git submodule init
 git submodule update
 
 
-cd $source_install_dir/conductor/src
-
 if [ $use_bundler ]; then
+  cd "$conductor_dir/src"
 
   # where the gems bundle pulls down will live:
-  mkdir -p $source_install_dir/bundler
+  mkdir -p "$conductor_dir/src/bundle"
 
   # only Gemfile (not Gemfile.in) should exist
   if [ -e Gemfile.in ]; then  mv -f Gemfile.in Gemfile; fi
 
   # pull down the gem dependencies
-  bundle install --path $source_install_dir/bundler
+  bundle install --path "$conductor_dir/src/bundle"
 
 else
+  cd "$conductor_dir/src"
 
   # only Gemfile.in (not Gemfile) should exist
   if [ -e Gemfile ]; then  mv -f Gemfile Gemfile.in; fi
@@ -35,14 +33,17 @@ else
 fi
 
 # In this example, we use postgres.
-cp $source_install_dir/conductor/src/config/database.pg \
- $source_install_dir/conductor/src/config/database.yml
-perl -p -i -e "s/username: aeolus/username: $dev_user/" \
-    $source_install_dir/conductor/src/config/database.yml
-# prefix our database names with our username so we don't clash
-# with another user (if another user is also doing development)
-perl -p -i -e "s/database: (conductor.*\$)/database: ${dev_user}_\$1/" \
-    $source_install_dir/conductor/src/config/database.yml
+cp "$conductor_dir/src/config/database.pg" \
+ "$conductor_dir/src/config/database.yml"
+
+if [[ "$aeolus_postgres_user" != "aeolus" ]] ; then
+  perl -p -i -e "s/username: aeolus/username: $dev_user/" \
+      $conductor_dir/src/config/database.yml
+  # prefix our database names with our username so we don't clash
+  # with another user (if another user is also doing development)
+  perl -p -i -e "s/database: (conductor.*\$)/database: ${dev_user}_\$1/" \
+      $conductor_dir/src/config/database.yml
+fi
 
 
 # keys for imagefactory
--- config.sh
+++ config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+# == USER ==
 export dev_user=aedev
 export dev_home="/home/$dev_user"
 
+
+# == RUBY ==
 # export ruby_version='1.8.7-p370'
 export ruby_version='1.9.3-p194'
 export ruby_gems='bundler rake pry'
-
-export source_install_dir="$dev_home"
 
 export use_bundler=true
 if [ $using_bundler ] ; then
@@ -17,6 +19,10 @@ fi
 
 export vm_hostname='f16-aeolus'
 
+
+# == CONDUCTOR ==
+export conductor_postgres_user=aeolus
+export conductor_dir="$dev_home/conductor"
 
 
 # DO NOT CHANGE THE REST OF THE FILE
